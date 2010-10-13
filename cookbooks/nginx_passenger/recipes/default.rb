341d71d6e8ef86776f684ed63059c8a2d65c51e5
Denis <denis.goeury@gmail.com>
2010-10-13 09:07:13 -0700

    choose worker count based on instance type

--- cookbooks/nginx_passenger/recipes/default.rb
+++ cookbooks/nginx_passenger/recipes/default.rb
@@ -4,6 +4,23 @@
 #
 if ['solo', 'util'].include?(node[:instance_role])
   
+  worker_count = case node[:ec2][:instance_type]
+    when "m1.small"
+      3
+    when "m1.large"
+      6
+    when "c1.medium"
+      6
+    when "c1.xlarge"
+      24
+    when "m1.xlarge"
+      12
+    when "m2.2xlarge"
+      8
+    when "m2.4xlarge"
+      24
+  end
+  
   node[:applications].each do |app, data|
     
     template "/data/nginx/stack.conf" do
@@ -13,7 +30,7 @@ if ['solo', 'util'].include?(node[:instance_role])
       source "nginx_stack.conf.erb"
       variables(
         :user => node[:owner_name],
-        :worker_count => get_pool_size()
+        :worker_count => worker_count
       )
     end
 
